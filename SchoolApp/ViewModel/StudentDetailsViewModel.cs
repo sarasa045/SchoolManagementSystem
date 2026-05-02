8b62b2120aab427fa1e67dc5b83a8c55c341c9ba
Revathi <revathidonthireddy7@gmail.com>
2026-05-02 11:18:32 +0530

    Updated view model in student details

---
+++ SchoolApp/ViewModel/StudentDetailsViewModel.cs
@@ -0,0 +1,92 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+
+namespace StudentApp.ViewModels
+{
+    public class StudentDetailsViewModel : INotifyPropertyChanged
+    {
+        private string firstName;
+        private string lastName;
+        private string age;
+        private string className;
+        private string section;
+        private string fatherName;
+        private string motherName;
+
+        public string FirstName
+        {
+            get => firstName;
+            set { firstName = value; OnPropertyChanged(); }
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set { lastName = value; OnPropertyChanged(); }
+        }
+
+        public string Age
+        {
+            get => age;
+            set { age = value; OnPropertyChanged(); }
+        }
+
+        public string ClassName
+        {
+            get => className;
+            set { className = value; OnPropertyChanged(); }
+        }
+
+        public string Section
+        {
+            get => section;
+            set { section = value; OnPropertyChanged(); }
+        }
+
+        public string FatherName
+        {
+            get => fatherName;
+            set { fatherName = value; OnPropertyChanged(); }
+        }
+
+        public string MotherName
+        {
+            get => motherName;
+            set { motherName = value; OnPropertyChanged(); }
+        }
+
+        public ICommand SaveCommand { get; }
+        public ICommand ClearCommand { get; }
+
+        public StudentDetailsViewModel()
+        {
+            SaveCommand = new RelayCommand(SaveStudent);
+            ClearCommand = new RelayCommand(ClearFields);
+        }
+
+        private void SaveStudent()
+        {
+            MessageBox.Show("Student details saved successfully");
+        }
+
+        private void ClearFields()
+        {
+            FirstName = "";
+            LastName = "";
+            Age = "";
+            ClassName = "";
+            Section = "";
+            FatherName = "";
+            MotherName = "";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
